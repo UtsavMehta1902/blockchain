281be62f85b7ebed2d07749c8c7ce69df77274ad
UtsavMehta1902 <utsavmehta2002@gmail.com>
2022-12-07 11:40:37 +0530

    Completed code and tests for single block generation.

--- block.test.js
+++ block.test.js
@@ -1,5 +1,6 @@
 const Block = require("./block");
 const { GENESIS_DATA } = require("./config");
+const cryptoHash = require("./crypto_hash");
 
 describe("Block", () => {
   const timestamp = "tmstmp";
@@ -18,12 +19,38 @@ describe("Block", () => {
   describe("genesis()", () => {
     const genBlock = Block.genesis();
 
-    it("returans a Block object", () => {
+    it("returns a Block object", () => {
       expect(genBlock instanceof Block).toEqual(true);
     });
 
     it("returns the genesis data as return value", () => {
       expect(genBlock).toEqual(GENESIS_DATA);
+    });
+  });
+
+  describe("mineBlock()", () => {
+    const lastBlock = Block.genesis();
+    const data = "random data";
+    const minedBlock = Block.mineBlock({ lastBlock, data });
+
+    it("returns a Block Object", () => {
+      expect(minedBlock instanceof Block).toEqual(true);
+    });
+
+    it("has a timestamp", () => {
+      expect(minedBlock.timestamp).not.toEqual(undefined);
+    });
+
+    it("has 'lastHash' equal to 'lastBlock->hash'", () => {
+      expect(minedBlock.lastHash).toEqual(lastBlock.hash);
+    });
+
+    it("has 'data' equal to the parameter provided", () => {
+      expect(minedBlock.data).toEqual(data);
+    });
+
+    it("generates a correct SHA-256 hash", () => {
+      expect(minedBlock.hash).toEqual(cryptoHash(minedBlock.timestamp, lastBlock.hash, data));
     });
   });
 });
