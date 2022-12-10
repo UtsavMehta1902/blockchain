4a155886fec4689e8c86423f360eb2dc187a4e7f
Utsav <utsavmehta2002@yahoo.com>
2022-12-11 01:40:19 +0530

    Added network functionality: pubsub and channels.

---
+++ index.js
@@ -0,0 +1,54 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const request = require("request");
+const Blockchain = require("./blockchain");
+const PubSub = require("./pubsub");
+
+const app = new express();
+const blockchain = new Blockchain();
+const pubsub = new PubSub(blockchain);
+const DEFAULT_PORT = 3000;
+
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
+
+// setTimeout(() => {
+//   pubsub.broadcastChain();
+// }, 1000);
+
+app.use(bodyParser.json());
+
+app.get("/api/blocks", (req, res) => {
+  res.json(blockchain.chain);
+});
+
+app.post("/api/mine", (req, res) => {
+  const { data } = req.body;
+  blockchain.addBlock({ data });
+  pubsub.broadcastChain();
+  res.redirect("/api/blocks");
+});
+
+const syncChain = () => {
+    request({url: `${ROOT_NODE_ADDRESS}/api/blocks`}, (error, response, body) => {
+        if(!error && response.statusCode === 200){
+            const response_chain = JSON.parse(body);
+            console.log("Syncing with existant blockchain.");
+            blockchain.updateChain(response_chain);
+        }
+    });
+}
+
+let PORT = DEFAULT_PORT;
+
+if (process.env.GENERATE_PEER_PORT === "TRUE")
+  PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
+
+app.listen(PORT, () => {
+    console.log(`listening to port-${PORT}`);
+
+    if(PORT !== DEFAULT_PORT)
+        syncChain();
+    // setTimeout(() => {
+    //     console.log(blockchain.chain)
+    // }, 1000);
+});
---
+++ pubsub.js
@@ -0,0 +1,55 @@
+const PubNub = require("pubnub");
+
+const credentials = {
+  publishKey: "pub-c-b26cbef7-3f83-46ee-8dde-a2d5db1a0b67",
+  subscribeKey: "sub-c-58859ffe-cbf7-47c3-a685-6a69b2fbefd2",
+  secretKey: "sec-c-MjUwY2EyZGEtYjk3OS00ZTYzLTg3NGUtMTc4NzIyMzIyMjA5",
+};
+
+const CHANNELS = {
+  TEST: "TEST-CHANNEL",
+  BLOCKCHAIN: "BLOCKCHAIN-CHANNEL",
+};
+
+class PubSub {
+  constructor( blockchain ) {
+    this.blockchain = blockchain;
+    this.pubnub = new PubNub(credentials);
+    this.pubnub.subscribe({ channels: Object.values(CHANNELS) });
+
+    this.pubnub.addListener(this.listener());
+  }
+
+  listener() {
+    return {
+      message: (messageObject) => {
+        const { message, channel } = messageObject;
+        console.log(
+          `Message recieved on channel: ${channel} and msg: ${message}`
+        );
+
+        const parsedMsg = JSON.parse(message);
+
+        if (channel === CHANNELS.BLOCKCHAIN) {
+          this.blockchain.updateChain(parsedMsg);
+        }
+      },
+    };
+  }
+
+  publish({ channel, message }) {
+    this.pubnub.publish({ channel, message });
+  }
+
+  broadcastChain() {
+    this.publish({
+      channel: CHANNELS.BLOCKCHAIN,
+      message: JSON.stringify(this.blockchain.chain),
+    });
+  }
+}
+
+// const pubsub = new PubSub();
+// pubsub.publish({ channel: CHANNELS.TEST, message: "jskvj" });
+
+module.exports = PubSub;
