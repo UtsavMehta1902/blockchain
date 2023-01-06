c2fdcb23e005a4335c5be6a43b0f838860217491
Utsav <utsavmehta2002@yahoo.com>
2023-01-06 22:18:10 +0530

    Changed localhost url to deployed url.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,7 +6,7 @@ const App = () => {
   const [walletInfo, setWalletInfo] = useState({ balance: 0, address: "-" });
 
   useEffect(() => {
-    fetch("http://localhost:3000/api/wallet-info", {
+    fetch("https://cryptowallet-6ijl.onrender.com/api/wallet-info", {
       method: "GET",
     })
       .then((res) => res.json())
